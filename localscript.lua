5ac2acc11b02826912118fbac05eaea680a7e767
bl6ndr <75757171+bl6ndr@users.noreply.github.com>
2024-12-16 11:17:39 -0800

    V1.0.1

--- localscript.lua
+++ localscript.lua
@@ -5,16 +5,20 @@ local player = Players.LocalPlayer
 local backpack = player:WaitForChild("Backpack")
 local toolsCheckerEvent = ReplicatedStorage:WaitForChild("ToolsChecker")
 
-local function Scanner()
-    local toolNames = {}
-    for _, item in ipairs(backpack:GetChildren()) do
-        if item:IsA("Tool") then
-            table.insert(toolNames, item.Name)
+task.spawn(function()
+    while task.wait(10) do
+        local success = pcall(function()
+            local toolNames = {}
+            for _, item in ipairs(backpack:GetChildren()) do
+                if item:IsA("Tool") then
+                    table.insert(toolNames, item.Name)
+                end
+            end
+            toolsCheckerEvent:FireServer(toolNames)
+        end)
+
+        if not success then
+            player:Kick("Error scanning tools")
         end
     end
-    toolsCheckerEvent:FireServer(player.Name, toolNames)
-end
-
-while task.wait(10) do
-    Scanner()
-end
+end)
